b5cbd50f5386d8f4bdcef7426b26afd2619c4da0
Armaan Sandhu <armaan.sandhu.cgy@gmail.com>
2026-06-10 17:59:53 -0600

    config v4

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,11 @@ import { qrcode } from "vite-plugin-qrcode";
 
 export default {
 	plugins: [Inspect(), qrcode()],
-	base: "/test-repo/",
+
 	build: {
 		rollupOptions: {
 			input: {
-				game1: "/game-1/index.html",
+				game1: "game-1/index.html",
 			},
 		},
 	},
